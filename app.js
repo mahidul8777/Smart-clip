ffcc147eb547c50ecff6504f8b22e0a40272ea4d
mahidul8777 <msworld8777@gmail.com>
2025-05-24 21:59:51 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* আপনার রাউটগুলি এখানে */}
+      </Routes>
+    </Router>
+  );
+}
